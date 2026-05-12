4d3e9b92e8fc731abd0ff2ab0d626379a053ab2d
ALIREZA MAK <info@alirezamak.com>
2026-05-12 12:00:54 -0700

    Created Sidebar and Map and added them to AppLayout

--- src/pages/AppLayout.tsx
+++ src/pages/AppLayout.tsx
@@ -1,3 +1,11 @@
+import Map from "../components/Map";
+import Sidebar from "../components/Sidebar";
+import styles from "./AppLayout.module.css";
 export default function AppLayout() {
-    return <div></div>;
+    return (
+        <div className={styles.app}>
+            <Sidebar />
+            <Map />
+        </div>
+    );
 }
