b955cb166b6ab465db48ee04d0daf11291923500
ALIREZA MAK <info@alirezamak.com>
2026-05-12 13:18:32 -0700

    Created CityList and added it as the child of app root

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,21 @@ import PageNotFound from "./pages/PageNotFound";
 import Pricing from "./pages/Pricing";
 import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
+import CityList from "./components/CityList";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Homepage />} />
+                <Route index element={<Homepage />} />
                 <Route path="/product" element={<Product />} />
                 <Route path="/pricing" element={<Pricing />} />
                 <Route path="/login" element={<Login />} />
-                <Route path="/app" element={<AppLayout />} />
+                <Route path="app" element={<AppLayout />}>
+                    <Route index element={<CityList />} />
+                    <Route path="cities" element={<p>cities</p>} />
+                    <Route path="countries" element={<p>countries</p>} />
+                </Route>
                 <Route path="*" element={<PageNotFound />} />
             </Routes>
         </BrowserRouter>
